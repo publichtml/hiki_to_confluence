d7e8470212168241c07be9ef253573edec477be4
Wakana Odagiri <publi9html@gmail.com>
2013-06-20 19:29:27 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HikiToConfluence::Application.config.secret_key_base = 'c7dedfda886621b91cb9ac181e6530d57f7f2cb00efdec81c074e75f6304a09c83a0692fd1f5afcb1e7e285fce24032315cf25390070e5744abe80c048574e83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HikiToConfluence::Application.config.session_store :cookie_store, key: '_hiki_to_confluence_session'
