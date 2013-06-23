32c20b2e1b777b72b18453088e54409c790a1490
Wakana Odagiri <publi9html@gmail.com>
2013-06-24 07:18:40 +0900

    Enable HikiConverter#convert to multiple lines conversion

--- lib/hiki_converter.rb
+++ lib/hiki_converter.rb
@@ -1,2 +1,15 @@
 class HikiConverter
+  def initialize(text)
+    raw_lines = text.split(/\n/)
+    @lines = raw_lines.map{ |line| HikiLine.new(line) }
+  end
+
+  def convert
+    converted_texts = []
+    @lines.each_with_index do |line, cnt|
+      converted_texts << line.convert
+    end
+
+    converted_texts.join("\n") + "\n"
+  end
 end
--- spec/lib/hiki_converter_spec.rb
+++ spec/lib/hiki_converter_spec.rb
@@ -1,4 +1,55 @@
 require "spec_helper"
 
 describe HikiConverter do
+  describe "#covert" do
+    subject{ HikiConverter.new(@text) }
+
+    context "convert headings" do
+      it "'!' => 'h1. '" do
+        @text = <<EOS
+!headings1
+[[link|http://example.com]]
+!!headings2
+*[[link2|http://example.com/link2]]
+**level2
+*** level3
+****level4
+!!!headings3
+#level1
+
+##level2-1
+##level2-2
+### level3
+#### level4
+!!!!headings4
+aaa'''bbb'''{{br}}cccc
+!!!!!headings5
+||td1|| td2 || td3{{br}}aaa||
+!!!!!!headings6
+EOS
+
+        expect(subject.convert).to eq <<EOS
+h1. headings1
+[link|http://example.com]
+h2. headings2
+* [link2|http://example.com/link2]
+** level2
+*** level3
+**** level4
+h3. headings3
+# level1
+
+## level2-1
+## level2-2
+### level3
+#### level4
+h4. headings4
+aaa*bbb*\\cccc
+h5. headings5
+|td1| td2 | td3\\aaa|
+h6. headings6
+EOS
+      end
+    end
+  end
 end
